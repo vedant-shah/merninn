fa9a9b5ff940f197634d26a0651b1362d00c08dc
Vedant Shah <vedantshah2001@gmail.com>
2022-03-06 08:46:36 +0530

    fixed alert position

--- mern_inn_frontend/src/components/Alerts.js
+++ mern_inn_frontend/src/components/Alerts.js
@@ -4,7 +4,7 @@ function Alerts(props) {
   return (
     props.alert &&
     <>
-      <div className={`alert alert-${props.alert.type}`} role="alert">
+      <div className={`alert alert-${props.alert.type}`} style={{ position: 'absolute', zIndex: '100', width: '100%' }}role="alert">
         {props.alert.message}
       </div>
     </>
