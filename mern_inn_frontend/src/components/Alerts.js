7ecf42015255093279d9a50eed07df13aa7af5a2
Vedant Shah <vedantshah2001@gmail.com>
2022-03-06 09:47:33 +0530

    changed alert position

--- mern_inn_frontend/src/components/Alerts.js
+++ mern_inn_frontend/src/components/Alerts.js
@@ -4,8 +4,9 @@ function Alerts(props) {
   return (
     props.alert &&
     <>
-      <div className={`alert alert-${props.alert.type}`} style={{ position: 'absolute', zIndex: '100', width: '100%' }}role="alert">
+      <div className={`alert alert-${props.alert.type} alert-dismissible`} style={{ position: 'absolute', zIndex: '999', width: '100%', top: '0' }}role="alert">
         {props.alert.message}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
     </>
   )
